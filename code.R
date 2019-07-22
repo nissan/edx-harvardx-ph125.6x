aadae16e4839f5f4ca641ce1eccbb616ebdcce69
Nissan Dookeran <nissan.dookeran@gmail.com>
2019-07-22 12:30:11 +1000

    code from new lesson

---
+++ code.R
@@ -0,0 +1,34 @@
+getwd()
+path <-system.file("extdata", package="dslabs")
+list.files(path)
+filename <- "murders.csv"
+fullpath <- file.path(path, filename)
+fullpath
+file.copy(fullpath, getwd())
+file.exists(filename)
+
+library(tidyverse)
+read_lines("murders.csv", n_max = 3)
+dat <- read_csv(fullpath)
+
+## From lesson plan
+library(dslabs)
+library(tidyverse)    # includes readr
+library(readxl)
+# inspect the first 3 lines
+read_lines("murders.csv", n_max = 3)
+# read file in CSV format
+dat <- read_csv(filename)
+#read using full path
+dat <- read_csv(fullpath)
+head(dat)
+#Ex：
+path <- system.file("extdata", package = "dslabs")
+files <- list.files(path)
+files
+filename <- "murders.csv"
+filename1 <- "life-expectancy-and-fertility-two-countries-example.csv"
+filename2 <- "fertility-two-countries-example.csv"
+dat=read.csv(file.path(path, filename))
+dat1=read.csv(file.path(path, filename1))
+dat2=read.csv(file.path(path, filename2))
